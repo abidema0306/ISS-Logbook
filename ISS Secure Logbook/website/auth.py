77e2f644094afd13c3db3e88597a8d4725a4b119
Jonny <93822303+JonnyAsh@users.noreply.github.com>
2022-09-06 10:09:10 +0100

    add pics

--- ISS Secure Logbook/website/auth.py
+++ ISS Secure Logbook/website/auth.py
@@ -5,23 +5,25 @@ from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
+
+
 
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
+        count=0
+        
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
    
@@ -48,8 +50,8 @@ def sign_up():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
+        elif len(email) < 10:
+            flash('Email must be greater than 10 characters.', category='error')
         elif len(first_name) < 3:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
